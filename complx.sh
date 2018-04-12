6a6d6ad6a8859f648e0a92ed075c777d8c1183af
Austin Adams <git@austinjadams.com>
2018-04-11 20:54:29 -0400

    Add almost-working complx script

---
+++ complx.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+[[ $(id -ng) != docker ]] && {
+    printf 'error: run this as:\n\n\tsudo -g docker ./complx.sh\n\ninstead\n' >&2
+    exit 2
+}
+
+exec docker run -it --rm -e "DISPLAY=$DISPLAY" -v /tmp/.X11-unix:/tmp/.X11-unix \
+                         -v "$(pwd)/workdir:/home/developer/workdir" \
+                         -v "$(pwd)/.complx:/home/developer/.config/complx" \
+                         ausbin/cs2110 bash
+                         #ausbin/cs2110 complx
