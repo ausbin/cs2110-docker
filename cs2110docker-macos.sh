9e9ed3cdc80c996a12112753f819bcb3640427a8
Austin Adams <git@austinjadams.com>
2018-05-20 12:18:57 -0400

    macos script: Make X11 forwarding work

--- cs2110docker-macos.sh
+++ cs2110docker-macos.sh
@@ -5,8 +5,8 @@ mkdir -p "$workdir" "$workdir/.complx"
 
 docker pull ausbin/cs2110
 
-exec docker run -it --rm -e "DISPLAY=:0" \
-                         --mount "type=bind,src=$DISPLAY,dst=/tmp/.X11-unix/X0" \
+xhost +localhost
+exec docker run -it --rm -e "DISPLAY=docker.for.mac.host.internal:0" \
                          -v "$workdir:/home/developer/workdir" \
                          -v "$workdir/.complx:/home/developer/.config/complx" \
                          ausbin/cs2110
